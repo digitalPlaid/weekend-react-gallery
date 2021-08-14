2f6419e9748e3e5d124b1c155047239786811169
Dan St. Aubin <staubindj@gmail.com>
2021-08-14 13:38:29 -0500

    set up the client side get route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+    let [galleryList, setGalleryList] = useState([])
+
+    useEffect(() => {
+        fetchGalleryList();
+    }, []);
+
+    const fetchGalleryList = () => {
+        axios({
+          method: 'GET',
+          url: '/gallery'
+        }).then(response => {
+            console.log('Successfully retrieved galleryList', response.data);
+            setGalleryList(response.data);
+        }).catch(error => {
+            console.log('Failed to GET galleryList: ', error);
+            alert('Failed to GET gallery');
+        })
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">Gallery of My Life</h1>
+            </header>
+            <p>Gallery goes here</p>
+            <img src="images/goat_small.jpg"/>
+        </div>
     );
 }
 
